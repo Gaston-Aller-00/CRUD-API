c93b6590ca801fd992f1d677cdc11050a060cde2
Gaston <gastonallerdev@gmail.com>
2024-04-07 18:26:18 -0300

    clean index

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,18 @@ import express, { Request, Response, Router } from "express";
 import dotenv from "dotenv";
 import mongoose, { Schema } from "mongoose";
 import routerCrud from "./routes/alumnoRoutes";
-import routerAuth from "./routes/authroutes";
-
+import routerAuth from "./routes/authRoutes";
+import { connect } from "./db/db";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
-const DB_URL = process.env.DB_URL;
-
-async function connect() {
-  try {
-    await mongoose.connect(DB_URL);
-    console.log("Connected to mongodb");
-  } catch (error) {
-    console.log(error);
-  }
-}
-
+//call F to connect DB
 connect();
 //home
-app.use("/auth",routerAuth);
-
+app.use("/auth", routerAuth);
 app.use("/", routerCrud);
-// app.use("/", routerAuth);
 
 app.listen(PORT, () => {
   console.log(`Server running in the port ${PORT}...`);
