af2fe6cfd91ed3139518184473453e2eea0b86c3
Gaston <gastonallerdev@gmail.com>
2024-04-07 18:26:40 -0300

    utils validateEmail

---
+++ src/utils/validateEmail.ts
@@ -0,0 +1,8 @@
+//validate email
+ const  validateEmail = (email: string): boolean =>{
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+  
+
+  export default validateEmail
